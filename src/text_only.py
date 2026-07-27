a71b2d4e5fd13a7d915d6ae6820b0b8b338bad16
yemenkrichen <yemenkrichenb9@gmail.com>
2026-07-27 11:02:55 -0600

    Delete src/text_only.py

--- src/text_only.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("boston_user_transitions.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-for user, activities in data.items():
-    for act in activities:
-        tweet_text = act.get("text")
-        print(tweet_text)
